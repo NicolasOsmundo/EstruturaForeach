4d4b1873959112226defd0426c1e50fb4e17b238
NicolasOsmundo <nicolasosmundo@hotmail.com>
2021-03-26 14:22:21 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class EstruturaForeach
+{
+    static void Main(string[] args)
+    {
+        string[] lista = { "Projeto", "Projetando", "Gp Projeto" };
+        foreach (string nome in lista)
+        {
+            Console.WriteLine(nome);
+        }
+        Console.Read();
+    }
+}
